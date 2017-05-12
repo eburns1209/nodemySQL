d782319561b87fdc48e3ebe8e6992a5f3f89ab99
eburns <eburns1209@gmail.com>
2017-05-12 09:39:03 -0400

    sql updated

--- Bamazon2.sql
+++ Bamazon2.sql
@@ -7,4 +7,19 @@ CREATE TABLE `products2` (
   `price` decimal(10,4) NOT NULL,
   `stock_quantity` int(10) NOT NULL,
   PRIMARY KEY (`item_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) 
+
+
+INSERT INTO `bamazon_db`.`products2`
+(`item_id`,
+`product_name`,
+`department_name`,
+`price`,
+`stock_quantity`)
+VALUES
+(<{item_id: }>,
+<{product_name: }>,
+<{department_name: }>,
+<{price: }>,
+<{stock_quantity: }>);
+
